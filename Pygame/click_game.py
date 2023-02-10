4bcf68278aafb70b83b7124693d8e07186200cc4
TheoHerzogManofGURTandBREAD <113529473+TheoHerzogManofGURTandBREAD@users.noreply.github.com>
2023-02-10 10:19:53 -0500

    Add files via upload

--- Pygame/click_game.py
+++ Pygame/click_game.py
@@ -12,3 +12,24 @@ def update():
     Squid.left= Squid.left + 2
     if Squid.left > WIDTH:
         Squid.right= 0
+def on_mouse_down(pos):
+    if Squid.collidepoint(pos):
+        print("Yuhhhhh")
+    if Squid.collidepoint(pos):
+        set_Squid_hurt()
+    else:
+        print("You missed me!")
+    if Squid.collidepoint(pos):
+        sounds.eep.play()
+
+
+
+
+
+def set_Squid_hurt():
+    Squid.image = 'character-2'
+    clock.schedule_unique(set_Squid_normal, 1.0)
+
+
+def set_Squid_normal():
+    Squid.image = 'character-1'
